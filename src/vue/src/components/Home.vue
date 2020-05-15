172605a54ada23394a845cd7750a0dc57ed1c6fa
slimshady20 <jayden6830@gmail.com>
2020-05-15 19:28:49 +0900

    0515 2020 19:15

---
+++ src/vue/src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id = "app">
+        <Layout>
+            <template #header="self">
+                <router-link to="/home"><h1 id="title">{{self.title}}</h1></router-link>
+                <router-link to="/join"> <span id="join">{{self.join}}</span></router-link><br>
+                <router-link to="/login"><span id="login">{{self.login}}</span></router-link>
+            </template>
+            <template #sidebar="self">
+                {{self.menu}}
+                <ul class="menu">
+                    <li v-for="i of sidebars" :key="i.menu">
+                    {{i.menu}}
+                    </li>
+                </ul>
+            </template>
+            <template #content>
+               <router-view/>
+
+            </template>
+            <template #footer="self">
+                <h3 id="footer">{{self.footer}}</h3>
+            </template>
+        </Layout>
+
+    </div>
+</template>
+
+<script>
+    import Layout from "../components/common/Layout.vue"
+    export default {
+        name: "Home",
+        components : {Layout},
+       data(){                  //    data : ()=>{
+            return {
+                sidebars: [
+                    {menu:'쓰기'},
+                    {menu:'목록'},
+                    {menu:'검색'},
+                    {menu:'수정'},
+                    {menu:'삭제'}
+                ]
+            }
+        }
+    }
+</script>
+    <style scoped>
+        ul.menu{
+            position: relative;
+            padding: 5px;
+            list-style: none;
+            font-style: italic;
+        }
+    </style>
---
+++ src/vue/src/components/common/Layout.vue
@@ -0,0 +1,101 @@
+<template>
+    <div id="wrapper">
+    <header>
+    <slot name="header"
+          :title="data1"
+            :login="data2"
+            :join="data3"></slot>
+    </header>
+    <aside id="sidebar">
+    <slot name="sidebar">
+
+    </slot>
+    </aside>
+    <section id="content">
+        <slot name="content">
+
+    </slot>
+    </section>
+    <footer>
+    <slot name="footer"
+          :footer="data5"></slot>
+    </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Layout",
+        data(){
+            return {
+                data1: "축구 정보 시스템",
+                data2: "로그인",
+                data3: "회원가입",
+                data4: "축구 경기 일정 검색",
+                data5: "(사)축구 선수 협회"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    /* 전체 구조 */
+    #wrapper {
+        padding: 5px;
+        width: 960px;
+        margin: 20px auto;
+    }
+    header {
+        height: 100px;
+        padding: 0 15px;
+    }
+    #content {
+        width: 696px;
+        float: left;
+        padding: 5px 15px;
+        min-height: 300px;
+    }
+    #sidebar {
+        width: 200px;
+        padding: 5px 15px;
+        float: left;
+    }
+    footer {
+        clear: both;
+        padding: 0 15px;
+    }
+    /* 가로폭 980보다 작을 때 사용할 @media query */
+    @media screen and (max-width: 980px) {
+        #pagewrap {
+            width: 94%;
+        }
+        #content {
+            clear: both;
+            padding: 1% 4%;
+            width: auto;
+            float: none;
+        }
+        #sidebar {
+            clear: both;
+            padding: 1% 4%;
+            width: auto;
+            float: none;
+        }
+        header, footer {
+            padding: 1% 4%;
+        }
+    }
+    /* 공통 */
+    #content {
+        background: #f8f8f8;
+    }
+    #sidebar {
+        background: #f0efef;
+    }
+    header, #content, #middle, #sidebar {
+        margin-bottom: 5px;
+    }
+    #pagewrap, header, #content, #middle, #sidebar, footer {
+        border: solid 1px #ccc;
+    }
+</style>
